c9302733b7cef352a3919a943104b5315d220211
Jack Hutchinson <jack.hutchinson@checkout.com>
2021-04-06 20:11:23 +0100

    Clean up

--- jest-config.js
+++ jest-config.js
@@ -3,7 +3,6 @@ module.exports = {
 	transform: {
 		'^.+\\.tsx?$': 'ts-jest',
 	},
-	setupFilesAfterEnv: ['@testing-library/react/cleanup-after-each', '@testing-library/jest-dom/extend-expect'],
 	testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
 	moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 };
